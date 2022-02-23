6feac863f3620a40c4088a9dbb1e60eab85c3e99
weissguy <weissp68@stanford.edu>
2022-02-22 17:23:03 -0800

    works

--- test_bae.py
+++ test_bae.py
@@ -5,20 +5,19 @@ from train import get_dataset
 from args import get_train_test_args
 from transformers import DistilBertTokenizerFast
 from transformers import DistilBertForQuestionAnswering
-from transformers import AdamW
 
 # define parser and arguments
-args = get_train_test_args()
-util.set_seed(args.seed)
-model = DistilBertForQuestionAnswering.from_pretrained("distilbert-base-uncased")
-tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
-if not os.path.exists(args.save_dir):
-    os.makedirs(args.save_dir)
-args.save_dir = util.get_save_dir(args.save_dir, args.run_name)
-log = util.get_logger(args.save_dir, 'log_train')
-log.info(f'Args: {json.dumps(vars(args), indent=4, sort_keys=True)}')
-log.info("Preparing Training Data...")
-args.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-train_dataset, _ = get_dataset(args, args.train_datasets, args.train_dir, tokenizer, 'train')
-for item in train_dataset:
-    print(item)
+def get_training_data(train_dataset="squad", train_dir="test_dataset"):
+    args = get_train_test_args()
+    util.set_seed(args.seed)
+    model = DistilBertForQuestionAnswering.from_pretrained("distilbert-base-uncased")
+    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+    args.save_dir = util.get_save_dir(args.save_dir, args.run_name)
+    log = util.get_logger(args.save_dir, 'log_train')
+    log.info(f'Args: {json.dumps(vars(args), indent=4, sort_keys=True)}')
+    log.info("Preparing Training Data...")
+    args.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    train_dataset, _ = get_dataset(args, train_dataset, train_dir, tokenizer, 'train')
+    return train_dataset
