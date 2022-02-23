49b52763afd1ed976e6a1a5ea60bb5f0b1aec164
Lynn Kong <lynndzkong@gmail.com>
2022-02-22 21:39:08 -0800

    ignore baseline and just generate samples by synonyms

--- bae/bae.py
+++ bae/bae.py
@@ -36,12 +36,14 @@ import numpy as np
 # >>> import nltk
 # >>> nltk.download('averaged_perceptron_tagger')
 from nltk import pos_tag
+from nltk.tokenize import WhitespaceTokenizer
 # TODO: note that we need to run the following one time in order for synset to work
 # >>> import nltk
 # >>> nltk.download('wordnet')
 from nltk.corpus import wordnet
 import util
 import torch
+import random
 
 class BERTAdversarialDatasetAugmentation:
     def __init__(
@@ -54,16 +56,36 @@ class BERTAdversarialDatasetAugmentation:
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
         self.MASK_CHAR = u"\u2047"
+        random.seed(224)
 
 ################################### PRIVATE ###################################
-    def _estimate_importance(self, sentence):
+    def _split_sentence(self, sentence_str):
+        """
+        Split a sentence string into array of word tokens.
+        Return the list of word tokens, and a map of the word token index to 
+            the offset in the original sentence string.
+        """
+        spans_generator = WhitespaceTokenizer().span_tokenize(sentence_str)
+        sentence = []
+        word_idx_to_offset = []
+        for span in spans_generator:
+            sentence.append(sentence_str[span[0]:span[1]])
+            word_idx_to_offset.append(span[0])
+        return sentence, word_idx_to_offset
+
+    def _estimate_importance(self, sentence, method='random'):
         """
         Estimates the importance of each token in the sentence, using
         the baseline model.
 
         Returns mask indices in DESCENDING order of importance.
         """
-        # TODO: Implement me!
+        if method == 'random':
+            idx_importance = [(i, 0.0) for i in range(len(sentence))]
+            return random.shuffle(idx_importance)
+        else:
+            # TODO: implement importance by baseline 
+            raise NotImplementedError
     
     def _pos_tags(self, sentence):
         return pos_tag(sentence)
@@ -86,24 +108,33 @@ class BERTAdversarialDatasetAugmentation:
         """
         return filter(lambda x: x[1] == tag, pos_tag(tokens))
 
-    def _baseline_fails(self, perturbed_sentences, label):
+    def _baseline_fails(self, perturbed_sentences):
         """
-        Given a list of perturbed sentences, returns the perturbed
-        sentences in which the model fails to correclty predict the label.
+        Given a list of perturbed sentence-answer pairs, returns the perturbed
+        sentence-answer pairs in which the model fails to correctly predict the answer.
         """
         return [
-            sentence for sentence in perturbed_sentences
-            if self.baseline.predict(sentence) != label # Probably need to update this TODO
+            sentence_answer for sentence_answer in perturbed_sentences
+            if self.baseline.predict(sentence_answer[0]) != sentence_answer[1] # Probably need to update this TODO
         ]
 
-    def _replace_mask(self, masked, token):
+    def _replace_mask(self, masked, token, word_idx_to_offset, answer_starts):
         """
-        Replace masked_character in masked with token
+        Replace masked_character in masked with token.
+        Return the new token list, as well as the updated answer starting index. 
         """
+        # TODO: we should skip any masking that is in the answer
+        mask_index = masked.index(self.MASK_CHAR)
+        new_answer_starts = []
+        for answer_start in answer_starts:
+            if answer_start > word_idx_to_offset[mask_index]:
+                # if answer starts after the replaced token, update the start
+                # to reflect the shift due to token length change.
+                new_answer_starts.append(answer_start + len(token) - (word_idx_to_offset[mask_index + 1] - word_idx_to_offset[mask_index]))
         return [
             token if item == self.MASK_CHAR else item
             for item in masked 
-        ]
+        ], new_answer_starts
 
     def _generate_mask(self, sentence, idx, BAE_TYPE='R'):
         if BAE_TYPE == 'R':
@@ -121,35 +152,48 @@ class BERTAdversarialDatasetAugmentation:
 # ----------------------------------------------------------------------------#
 ################################### PUBLIC ####################################
 
-    def perturb(self, sentence, label, BAE_TYPE='R'):
+    def perturb(self, sentence_str, answer_starts, BAE_TYPE='R', use_baseline=False):
         """
         Implements the perturbation algorthim described above.
         Based off of the BAE-R algorithm.
+        Returns a list of perturbed sentence strings and their answer indices.
         """
-        importances = self._estimate_importance(sentence) # I (paper)
+        # split sentence into word spans using
+        sentence, word_idx_to_offset = self._split_sentence(sentence_str)
+        importance_method = 'baseline' if use_baseline else 'random'
+        importances = self._estimate_importance(sentence, method=importance_method) # I (paper)
         tags = self._pos_tags(sentence)
 
-        for (importance, idx) in importances:
+        perturbation_results = []
+
+        for (idx, importance) in importances:
             tag = tags[idx]
             masked = self._generate_mask(sentence, idx, BAE_TYPE)
             # 
             tokens = self._predict_top_k(masked, tag) # T (paper)
             filtered_tokens = self._filter_tokens(tokens, tag)
             perturbed_sentences = [ # L (paper)
-                self._replace_mask(masked, token)
+                self._replace_mask(masked, token, word_idx_to_offset, answer_starts)
                 for token in filtered_tokens
             ]
 
             # This is our deviation from BAE!
             # ignore sentences that don't cause baseline to fail
             # TODO: Discuss as group. Should we also allow cases where it doesn't fail?
-   
-            perturbed_and_baseline_fails = self._baseline_fails(perturbed_sentences, label)
+            if use_baseline:
+                perturbed_and_baseline_fails = self._baseline_fails(perturbed_sentences)
+            else:
+                # just use all of them
+                perturbed_and_baseline_fails = perturbed_sentences
 
             # If no perturbation works, move to the next most important mask.
             if perturbed_and_baseline_fails:
+                # Create the sentence strings from the perturbed sentence list as well as the original sentence.
+                sim_input_delimited = [sentence] + [sentence_answer[0] for sentence_answer in perturbed_and_baseline_fails]
+                sim_input = [" ".join(word_list) for word_list in sim_input_delimited]
                 # Get embeddings from the similarity scorer
-                embeddings = self.semantic_sim.encode([sentence] + perturbed_and_baseline_fails)
+                embeddings = self.semantic_sim.encode(sim_input)
+                # first embedding is of the original sentence.
                 original_embedding = embeddings[:1] # 1 x embed_size matrix
                 perturbed_embeddings = embeddings[1:] # |perturbed_sentences| x embed_size matrix
                 # score() returns 1 x |perturbed_sentences| matrix of similarities by inner product.
@@ -157,10 +201,10 @@ class BERTAdversarialDatasetAugmentation:
                 scores = self.semantic_sim.score(original_embedding, perturbed_embeddings, method='inner').flatten()
                 best_sentence_index = np.argmax(scores)
                 
-                return perturbed_and_baseline_fails[best_sentence_index]
+                perturbation_results.append(perturbed_and_baseline_fails[best_sentence_index])
         
         # Unable to find a good perturbation!
-        return []
+        return perturbation_results
 
     def perturb_dataset(self, dataset, bae_type='R'):
         """
@@ -168,7 +212,7 @@ class BERTAdversarialDatasetAugmentation:
         compute a new dataset, with perturbations!
         """
 
-        for (sentence, label) in dataset:
-            yield self.perturb(sentence, label, BAE_TYPE=bae_type)
+        for (sentence, answer_starts) in dataset:
+            yield self.perturb(sentence, answer_starts, BAE_TYPE=bae_type)
 
 ################################### PUBLIC ####################################
--- test_bae.py
+++ test_bae.py
@@ -5,12 +5,13 @@ from train import get_dataset
 from args import get_train_test_args
 from transformers import DistilBertTokenizerFast
 from transformers import DistilBertForQuestionAnswering
+from bae.bae import BERTAdversarialDatasetAugmentation
+from semantic_similarity_scorer.sbert import SBERTScorer
 
 # define parser and arguments
-def get_training_data(train_dataset="squad", train_dir="test_dataset"):
+def get_training_data():
     args = get_train_test_args()
     util.set_seed(args.seed)
-    model = DistilBertForQuestionAnswering.from_pretrained("distilbert-base-uncased")
     tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
@@ -19,5 +20,20 @@ def get_training_data(train_dataset="squad", train_dir="test_dataset"):
     log.info(f'Args: {json.dumps(vars(args), indent=4, sort_keys=True)}')
     log.info("Preparing Training Data...")
     args.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    train_dataset, _ = get_dataset(args, train_dataset, train_dir, tokenizer, 'train')
-    return train_dataset
+    _, data_dict = get_dataset(args, args.train_datasets, args.train_dir, tokenizer, 'train')
+    return data_dict
+
+def get_perturbed_sentences(data_dict, perturber):
+    for i, context in data_dict['context']:
+        # TODO: generate and pass in answer_end indices 
+        perturbation_results = perturber.perturb(context, data_dict['answer'][i])
+        print(perturbation_results)
+
+def main():
+    data_dict = get_training_data()
+    perturber = BERTAdversarialDatasetAugmentation(None, None, SBERTScorer(), 10)
+    get_perturbed_sentences(data_dict, perturber)
+    # TODO write the perturbed sentences to disk
+
+if __name__ == '__main__':
+    main()
